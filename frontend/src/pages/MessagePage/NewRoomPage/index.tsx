b48b6bed18a0ab3838a9b536b86a221790b6c2ea
Eric Do <beatyshot@gmail.com>
2021-12-27 17:01:48 +1000

    new room layout

--- frontend/src/pages/MessagePage/NewRoomPage/index.tsx
+++ frontend/src/pages/MessagePage/NewRoomPage/index.tsx
@@ -1,6 +1,36 @@
+import { Autocomplete, InputAdornment, TextField } from "@mui/material"
+
+const options = [
+  { label: 'Yuki', value: 'User:1' },
+  { label: 'Miku', value: 'User:2' },
+]
+
 const NewRoomPage = () => {
   return (
-    <>new room page</>
+    <Autocomplete
+      multiple
+      options={options}
+      getOptionLabel={(option) => option.label}
+      defaultValue={[options[0]]}
+      fullWidth
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          InputProps={{
+            ...params.InputProps,
+            startAdornment: (
+              <InputAdornment position='start'>
+                To:
+                <span style={{ marginLeft: 8 }}>
+                  {params.InputProps.startAdornment}
+                </span>
+              </InputAdornment>
+            ),
+            endAdornment: undefined
+          }}
+        />
+      )}
+    />
   )
 }
 
